245e2033f209033b4de894628297776b4b069341
dsalv <dias@dune.xyz>
2024-03-31 16:17:52 +0200

    Modified foundry test files

--- test/Fork_Simulate.sol
+++ test/Fork_Simulate.sol
@@ -33,20 +33,20 @@ contract ForkSimulate is DSTest {
     function testTransact() public {
 
         // Enter forking mode at block: https://etherscan.io/block/{block_number}
-        uint256 fork = vm.createFork("https://rpc.notadegen.com/eth", 15596646);
+        uint256 fork = vm.createFork("https://rpc.notadegen.com/eth", 15596643);
         vm.selectFork(fork);
 
         // a random transfer transaction in the block: https://etherscan.io/tx/0xaba74f25a17cf0d95d1c6d0085d6c83fb8c5e773ffd2573b99a953256f989c89
-        bytes32[17] memory tx_array = [bytes32(0xf49ab4cd174d7ca415a9e63e93631843f8999a86fb0966503d8f3316fd9832ab), bytes32(0xc1706daf676d1c8ed4e6f596a7c0e5d3105d490da2ba25cd68ec224b32284df7), bytes32(0xaaba9ab9e765f0df40e54dc5082ed1b03175895175a86b8b5a2c991617937daf), bytes32(0x53b323c91d25e7d85694e599a0d16b54fc8e540012b7d4f18d0b09fc110b74f8), bytes32(0xb482d026f7a72097030e2257725d982448bdddae055e1d9f9f834348bd022af0), bytes32(0xc1bbe6fa7e7c9829898fe92da2214130ea865dcb8cc4ac7140534f468f6a549d), bytes32(0x966c1ad69d88a751f2eeb96390df1c0b93b59748f0ec2a38bc2f8e3df26d6869), bytes32(0xd1f8a5def0074904cc0afd154150cfb1fbf8ed3160c4812e71953b8133ed36b3), bytes32(0x1e758acc32240f29ff66df698aae786e2ab6609fcee3b8b007265b3324340908), bytes32(0x3d2579dd48ded7852079cc207f42627340a907aa9527ff52ffdfcd52b74c8099), bytes32(0xc3f54071a0b754003ecb214068f9854b6c412f523b4f87a9dcb57b1288bc9a0b), bytes32(0x00927449d3f6f3f6324a31a3dc65b859f0953f9d73f615574f4e00d8ed26c414), bytes32(0x6ff35727688ea911552cf095ed805f28ed83c0e571d3a9abfcc226544753b8ba), bytes32(0xd3206e8d1ac8bdbbc1cf8f8ce511517fbbee53d443a6da34f08edb88043f9357), bytes32(0xb1b025011c8a2a021ddff23a2954203e9cd9191a404f9eba76e8c8526862795c), bytes32(0x50abb38c8d3fe3efed76402fcc5118331dd7bb148c8aa723a1220eabab500e32), bytes32(0x485aaefb176c089f7b868f8ffe3be56cf24b34c07f00597d5a3c57cbe309521d)];
+        bytes32[14] memory tx_array = [bytes32(0x9ce6e18d8fa53a926dcf86dda8d8213914c0c08577700f8d2009da0f6110d66d), bytes32(0xc8ec642a34cee311a8d103e067d0441cb6799a9fcbd610d21b53c25bede22358), bytes32(0x9139dd7337e4e5a0526124cdf7d0af5a125db3a2071fd6dd4b79ca50e2ee3d90), bytes32(0x1f8c7813f05db8d704330c32e1dbc8fc52fa654cd40c9ec484e557f1a3726313), bytes32(0xad0abcbac159eba56338a7fefda0640024991132887976a1d17267a816ceb852), bytes32(0xaca961db782691ee52f932290fe2d91d2f71f42dd894846f9f06995aeb8c4796), bytes32(0x7b8d5ea1b62057293e8fa941785de60c83f14a9f76e00aac08db3ebcb67ecffa), bytes32(0x1390f9c3bcc87f6c59af2c34da2342a651616713790e890e394ebe8ed42ef8f5), bytes32(0x1fed490b75c3b90ead6699833dd913d8a7b3bcaeff51629c8adb8ffaefbd9d29), bytes32(0x6521bf5baf6806e24ec77b95f571b6c9a6df91dc4ecd66fd1951ff3e082b7baa), bytes32(0x9d15954fef8a4e40f3d067a04a98a15c3fe357d6ae4a6b80be9c22d239a69e60), bytes32(0x64d2ad159892f5b15e9b335334511392b8d9e0a9686d789c3683f620c952a6ca), bytes32(0xa94f2559d2177587c3aa3beef70055bbee35b6e6d5a1451458f455442eaf6299), bytes32(0x9a2b6847c63ce6cf859a67af5d8ec7b2c2642032b5ed4a947e1562ea019dc422)];
         // traders
-        address[17] memory senders = [address(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D), address(0x56178a0d5F301bAf6CF3e1Cd53d9863437345Bf9), address(0x1E888882D0F291DD88C5605108c72d414f29D460), address(0x82B771E9F2F9B92B4B8f4EBDA4aeB60d3040d6Dc), address(0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc), address(0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F), address(0x1111111254fb6c44bAC0beD2854e76F90643097d), address(0xe7bc275b8e5474461f605A2fA58FE534DDdad8B5), address(0x1111111254fb6c44bAC0beD2854e76F90643097d), address(0x3f555754820bf48f89f53a3CC3E039da95f9647F), address(0x1111111254fb6c44bAC0beD2854e76F90643097d), address(0x5C65EFBCE63FA52A2aE056aadcA9e9655eA388ed), address(0x98C3d3183C4b8A650614ad179A1a98be0a8d6B8E), address(0xD017EA99F60535e5e0f87f997968ad59C6a61B06), address(0x98C3d3183C4b8A650614ad179A1a98be0a8d6B8E), address(0x56178a0d5F301bAf6CF3e1Cd53d9863437345Bf9), address(0xBEEFBaBEeA323F07c59926295205d3b7a17E8638)];
-        // reouters
-        address[17] memory recipients = [address(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D), address(0xa57Bd00134B2850B2a1c55860c9e9ea100fDd6CF), address(0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F), address(0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45), address(0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45), address(0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F), address(0x1111111254fb6c44bAC0beD2854e76F90643097d), address(0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45), address(0x1111111254fb6c44bAC0beD2854e76F90643097d), address(0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45), address(0x1111111254fb6c44bAC0beD2854e76F90643097d), address(0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45), address(0x98C3d3183C4b8A650614ad179A1a98be0a8d6B8E), address(0x25553828F22bDD19a20e4F12F052903Cb474a335), address(0x98C3d3183C4b8A650614ad179A1a98be0a8d6B8E), address(0xa57Bd00134B2850B2a1c55860c9e9ea100fDd6CF), address(0xBEEFBaBEeA323F07c59926295205d3b7a17E8638)];
+        address[14] memory senders = [address(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D), address(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D), address(0x98C3d3183C4b8A650614ad179A1a98be0a8d6B8E), address(0x74de5d4FCbf63E00296fd95d33236B9794016631), address(0x7398A7604F03D62B0bd0440198cAe779C8F1481D), address(0x74de5d4FCbf63E00296fd95d33236B9794016631), address(0x03B5677515A54479e1af3F2BAf1eF3e995b7a8E9), address(0x6056C80201718655EC8F3315165925a20BF1A636), address(0x794DceEDd37e140553C161Fe230Ea80c76cdB622), address(0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45), address(0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45), address(0x1111111254fb6c44bAC0beD2854e76F90643097d), address(0x56178a0d5F301bAf6CF3e1Cd53d9863437345Bf9), address(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D)];
+        // routers
+        address[14] memory recipients = [address(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D), address(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D), address(0x98C3d3183C4b8A650614ad179A1a98be0a8d6B8E), address(0xDef1C0ded9bec7F1a1670819833240f027b25EfF), address(0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45), address(0x1111111254fb6c44bAC0beD2854e76F90643097d), address(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D), address(0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45), address(0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45), address(0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45), address(0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45), address(0x1111111254fb6c44bAC0beD2854e76F90643097d), address(0xa57Bd00134B2850B2a1c55860c9e9ea100fDd6CF), address(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D)];
 
         // tokens sold
-        address[17] memory tokens_sold = [0x7b78B5aBE48e0a0377e463aFb7Ad434bBe9c8c92, 0x6B175474E89094C44Da98b954EedeAC495271d0F, 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, 0x8a34D707189fBFa46930ABFDe3D22abDC48AB73D, 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48, 0x695106Ad73f506f9D0A9650a78019A93149AE07C, 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, 0x4E15361FD6b4BB609Fa63C81A2be19d873717870, 0x664C6E221c77313307467B121528ad563107bD01, 0x6B175474E89094C44Da98b954EedeAC495271d0F, 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48, 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, 0xdAC17F958D2ee523a2206206994597C13D831ec7, 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48, 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48];
+        address[14] memory tokens_sold = [0xC6c1468fD63ED0652D3404E90E87dACF403e299D, 0x8dBF6A9E8fA47ce7696218551Aa471C284c8ece2, 0x7DD9c5Cba05E151C895FDe1CF355C9A1D5DA6429, 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, 0x1Cc29Ee9dd8d9ed4148F6600Ba5ec84d7Ee85D12, 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, 0x87DC4F4d43de030A003763e4A1fCd48a28e8dc25, 0xF725f73CAEe250AE384ec38bB2C77C38ef2CcCeA, 0x41bc3E37DC7e737B6123868857479e369d19714e, 0x3AdcA048c5454FcF1411c940Ba0eD889ab7D7b2E, 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, 0x9773A5a526c9598EB4462f8158b4410113677195];
         // tokens bought
-        address[17] memory tokens_bought = [0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48, 0xAb56C0DbBd82E86b022289B441fCB8826cAFf65D, 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48, 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, 0x7b78B5aBE48e0a0377e463aFb7Ad434bBe9c8c92, 0x4E15361FD6b4BB609Fa63C81A2be19d873717870, 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, 0x7b78B5aBE48e0a0377e463aFb7Ad434bBe9c8c92, 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48, 0x4d224452801ACEd8B2F0aebE155379bb5D594381, 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2];
+        address[14] memory tokens_bought = [0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48, 0xc944E90C64B2c07662A292be6244BDf05Cda44a7, 0xB087C2180e3134Db396977065817aed91FEa6EAD, 0xE1BDA0c3Bfa2bE7f740f0119B6a34F057BD58Eba, 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, 0xB455007bC0Fd7B21cB21DEe9b3cCC4C4C43f0e1E, 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48, 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984, 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2];
 
         for (uint i = 0; i < tx_array.length; i++) {
             address sender = senders[i];
@@ -58,26 +58,18 @@ contract ForkSimulate is DSTest {
             TokenInterface token_sold = TokenInterface(tokens_sold[i]);
 
 
-            console.log("initial sender ETH balance: ", sender.balance);
-            console.log("initial sender balance: ", token_sold.balanceOf(sender));
+            console.log("Initial sender ETH balance: ", sender.balance);
+            console.log("Initial sender balance: ", token_sold.balanceOf(sender));
             uint256 shitcoinBalance = token_sold.balanceOf(sender);
-            console.log("initial sender shitcoin (token bought) balance: ", token_bought.balanceOf(sender));
+            console.log("Initial sender shitcoin (token bought) balance: ", token_bought.balanceOf(sender));
 
             //execute the transaction
-            // write try and catch in solidity
-            try vm.transact(tx_array[i]) {
-                console.log("Transaction successful");
-            } catch Error(string memory reason) {
-                console.log("Transaction failed: ", reason);
-            } catch (bytes memory lowLevelData) {
-                // console.log("Transaction failed: ", lowLevelData);
-            }
-            // vm.transact(tx_array[i]);
+            vm.transact(tx_array[i]);
         
-            console.log("final sender ETH balance: ", sender.balance);
-            console.log("final sender token sold balance: ", token_sold.balanceOf(sender));
+            console.log("Final sender ETH balance: ", sender.balance);
+            console.log("Final sender token sold balance: ", token_sold.balanceOf(sender));
             uint256 newShitcoinBalance = token_bought.balanceOf(sender);
-            console.log("final sender shitcoin (token bought) balance: ", newShitcoinBalance);
+            console.log("Final sender shitcoin (token bought) balance: ", newShitcoinBalance);
 
             uint256 shitcoinBalanceDifference = newShitcoinBalance - shitcoinBalance;
             console.log("The shitcoin difference: ", shitcoinBalanceDifference);
